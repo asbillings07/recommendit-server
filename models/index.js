06194629d30d5e2162bcb6c98c50647242c7f22c
AaronBillings <aaronbillings@abdevelops-linux.attlocal.net>
2020-04-28 13:22:34 -0400

    update index file for postgress

--- models/index.js
+++ models/index.js
@@ -1,51 +1,60 @@
-'use strict'
+'use strict';
 // gives access to the node env
-require('dotenv').config()
-const fs = require('fs')
-const path = require('path')
-const Sequelize = require('sequelize')
-const basename = path.basename(__filename)
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
 // will look in env or development
-const env = process.env.NODE_ENV || 'production'
-const config = require('../Config')[env]
-const db = {}
+const env = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-let sequelize
+let sequelize;
 // checks if any env variable is set
-if (config.databaseUrl) {
-  console.log(config.databaseUrl)
+if (config.use_env_variable) {
   // if it is use the settings for that
-  sequelize = new Sequelize(config.databaseUrl, config)
+  console.log(config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    rejectUnauthorized: false,
+  });
 } else {
   // if not use a new instance the db
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 // let's us know if our DB connection is good or not
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.')
+    console.log('Connection has been established successfully.');
   })
   .catch((err) => {
-    console.log('Unable to connect to the database:', err)
-  })
+    console.log('Unable to connect to the database:', err);
+  });
 // reading the files and importing sequelize
 fs.readdirSync(__dirname)
   .filter((file) => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
   })
   .forEach((file) => {
-    const model = sequelize['import'](path.join(__dirname, file))
-    db[model.name] = model
-  })
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 // allows us to use associatation or relationships for our db
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
-    db[modelName].associate(db)
+    db[modelName].associate(db);
   }
-})
+});
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = db
+module.exports = db;
