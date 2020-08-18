de5af854d23fdec89cf9911890a611a4f638dfac
asbillings07 <superswiftdeveloper@gmail.com>
2020-08-18 18:00:16 -0400

    updated rating functions and routes

--- routes/rating.js
+++ routes/rating.js
@@ -1,35 +1,35 @@
-const { authenticateUser } = require('../app');
-const express = require('express');
-const router = express.Router();
-const { validateRating } = require('../services/validationChain');
-const asyncHandler = require('../services/asyncErrorHanlder');
+const { authenticateUser } = require('../app')
+const express = require('express')
+const router = express.Router()
+const { validateRating } = require('../services/validationChain')
+const asyncHandler = require('../services/asyncErrorHanlder')
 const {
   createRating,
-  verifyUser,
+  findRatingByRecId,
   updateRating,
   deleteRating,
-  getRatings,
-} = require('../services/ratingFunctions');
+  getRatings
+} = require('../services/ratingFunctions')
 
 // GET /rating status 200 - gets all ratings for user
 router.get('/rating', authenticateUser, async (req, res) => {
-  const userId = req.user.id;
-  const ratings = await getRatings(userId);
-  res.status(200).json(ratings);
-});
+  const userId = req.user.id
+  const ratings = await getRatings(userId)
+  res.status(200).json(ratings)
+})
 // POST /rating/recs/:id status: 201 - creating a new rating for a given recommendation
 router.post(
   '/rating/recs/:id',
   authenticateUser,
   validateRating,
   asyncHandler(async (req, res) => {
-    const body = req.body;
-    const id = +req.params.id;
-    const user = req.user;
-    const rating = await verifyUser(id);
-    console.log(rating);
-    await createRating(id, user, body);
-    res.status(201).end();
+    const body = req.body
+    const id = +req.params.id
+    const user = req.user
+    const rating = await findRatingByRecId(id)
+    console.log(rating)
+    await createRating(id, user, body)
+    res.status(201).end()
     // if (rating.userid !== user.id) {
 
     // } else {
@@ -39,46 +39,54 @@ router.post(
     //   });
     // }
   })
-);
+)
 // PUT /rating/recs/:id - status: 204 - updates a rating for an existing recommendaion if the user owns the rating - returns no content.
 router.put(
   '/rating/recs/:id',
   authenticateUser,
   validateRating,
   asyncHandler(async (req, res) => {
-    const body = req.body;
-    const id = +req.params.id;
-    const user = req.user;
-    const authedUser = await verifyUser(id);
-    if (authedUser.userid === user.id) {
-      await updateRating(id, body);
-      res.status(204).end();
+    const { body, params, user } = req
+    const { id } = params
+    const rating = await findRatingByRecId(id)
+
+    if (user) {
+      if (rating !== null) {
+        await updateRating(id, body)
+        res.status(200).json({
+          message: 'rating updated!'
+        })
+      } else {
+        await createRating(id, user, body)
+        res.status(200).json({
+          message: 'rating created!'
+        })
+      }
     } else {
       res.status(403).json({
-        error: '403 Forbidden',
-        message: 'You can not edit ratings or comments that you do not own',
-      });
+        message: 'You can not edit ratings or comments if you are not logged in'
+      })
     }
   })
-);
+)
 // DELETE /rating/recs/:id - status: 204 - deletes a rating for an existing recommendaion if the user owns the rating - returns no content.
 router.delete(
   '/rating/recs/:id',
   authenticateUser,
   asyncHandler(async (req, res) => {
-    const id = +req.params.id;
-    const user = req.user;
-    const rating = await verifyUser(id);
+    const id = +req.params.id
+    const user = req.user
+    const rating = await findRatingByRecId(id)
     if (rating.userid === user.id) {
-      await deleteRating(id);
-      res.status(204).end();
+      await deleteRating(id)
+      res.status(204).end()
     } else {
       res.status(403).json({
         error: '403 Forbidden',
-        message: 'You can not delete ratings or comments that you do not own',
-      });
+        message: 'You can not delete ratings or comments that you do not own'
+      })
     }
   })
-);
+)
 
-module.exports = router;
+module.exports = router
